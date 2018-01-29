38e0d2ce18dfc35f5c023e7e1f11009dc674e869
Xiao <gx26@drexel.edu>
2018-01-29 14:57:23 -0500

    H2

---
+++ H2/Problem1.c
@@ -0,0 +1,24 @@
+#include "csapp.h"
+
+int main()
+{
+
+	int fd1, fd2;
+	
+	// descriptors 0, 1, 2 are taken by stdin, stdout, and stderr respectively
+	
+	fd1 = Open("foo.txt," O_RDONLY, 0); // Open "foo.txt", fd1 is descriptor 3
+	
+	fd2 = Open("bar.txt", O_RDONLY, 0); // Open "bar.txt", fd2 is descriptor 4
+	
+	Close(fd2); // Close fd2 file, the resources associated with this file are destroyed
+	// file descriptor 3 is now available
+	
+	fd2 = Open("baz.txt", O_RDONLY, 0); // Open "baz.txt"
+	// fd2 is descriptor 3 because 3 is the smallest descriptor available
+	
+	printf("fd2 = %d\n", fd2); // "fd2 = 3" because fd2 is file descriptor 3
+	
+	exit(0);
+
+}
