8f969f84218308ba8fc3d0c7f525ea3775bd50fa
Geoffrey Xiao <gx26@drexel.edu>
2018-02-06 09:21:03 -0500

    L2

---
+++ L2/L2.c
@@ -0,0 +1,18 @@
+/*
+
+		Take two arguments ./a and ./b.
+		If a file in a does not exist in b, replicate the file in a into b
+		If a file in b does not exist in a, it should be deleted from b
+		If a file exists in both directories a and b, the file with the most
+		recent modified date/time should be copied from one directory to the
+		other
+		Print a log of the program's activities to stdout or stderr
+
+*/
+
+int main(int argc, char** argv)
+{
+
+	
+
+}
