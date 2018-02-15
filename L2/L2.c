c63c54a20531df41eeacf7e902c195d425d49ba4
Geoffrey Xiao <gx26@drexel.edu>
2018-02-15 18:53:09 -0500

    L2.c

--- L2/L2.c
+++ L2/L2.c
@@ -29,12 +29,10 @@ int Get_Files(DIR* dir, char** files);
 void Get_Full_Path(char* directory, char** files, int num_files, char** files_out);
 int Get_Reg_Files(char* directory, char** files, int num_files);
 bool File_Exists(char* file, char** array, int array_size);
-int Files_To_Copy(char** from_dir_files, char** from_dir_full_path, int from_dir_size, 
-					   char** to_dir_files, char** to_dir_full_path, int to_dir_size, 
+int Files_To_Copy(char** from_dir_files,char* from_dir, int from_dir_size, 
+					   char** to_dir_files, char* to_dir, int to_dir_size, 
 						char** from_out, char** to_out);
 void Copy_From_To(char* from, char* to);
-void Get_Dir_From_Path(char* path, char* delim, char* out);
-void Get_File_From_Path(char* path, char* delim, char* out);
 
 
 int main(int argc, char** argv)
@@ -46,15 +44,18 @@ int main(int argc, char** argv)
 		exit(1);
 	}
 
-	// Init variables
+	// Get directory names, check to make sure that they are directories
 	char* dir_a_name = argv[1]; char* dir_b_name = argv[2];
+	if( dir_a_name[strlen(dir_a_name) - 1] != '/' ||
+	 	 dir_b_name[strlen(dir_b_name) - 1] != '/' )
+	{
+		fprintf(stderr, "Directory Not Passed\n");
+		exit(1);
+	}
+
+	// Init variables
 	DIR* dir_a; DIR* dir_b;
 	char* dir_a_files[MAX_NUM_FILES]; char* dir_b_files[MAX_NUM_FILES];
-	char* dir_a_files_full_path[MAX_NUM_FILES];
-	char* dir_b_files_full_path[MAX_NUM_FILES];
-
-	if( strcmp(dir_a_name[strlen(dir_a_name)], "/") != 0 )
-
 
 	// Open directories
 	dir_a = Open_Dir(dir_a_name); dir_b = Open_Dir(dir_b_name);
@@ -66,77 +67,35 @@ int main(int argc, char** argv)
 	dir_a_num_files = Get_Reg_Files(dir_a_name, dir_a_files, dir_a_num_files);	
 	dir_b_num_files = Get_Reg_Files(dir_b_name, dir_b_files, dir_b_num_files);	
 
-	Get_Full_Path(dir_a_name, dir_a_files, dir_a_num_files, dir_a_files_full_path);
-	Get_Full_Path(dir_b_name, dir_b_files, dir_b_num_files, dir_b_files_full_path);
-
-	for(int i = 0; i < dir_a_num_files; i++)
-	{
-		fprintf(stdout, "%s: %s\n", dir_a_files[i], dir_a_files_full_path[i]);
-	}
-
-	printf("%d\n", dir_b_num_files);
-	for(int i = 0; i < dir_b_num_files; i++)
-	{
-		fprintf(stdout, "%s: %s\n", dir_b_files[i], dir_b_files_full_path[i]);
-	}
-
-	char* copy_from_a[MAX_NUM_FILES]; // Which files should we copy from a to b
+	// Which files should we copy from a to b
+	char* copy_from_a[MAX_NUM_FILES]; 	
 	char* copy_to_b[MAX_NUM_FILES];
-	int size_a_to_b = Files_To_Copy(dir_a_files, dir_a_files_full_path, dir_a_num_files, 
-											  dir_b_files, dir_b_files_full_path, dir_b_num_files, 
+	int size_a_to_b = Files_To_Copy(dir_a_files, dir_a_name, dir_a_num_files, 
+											  dir_b_files, dir_b_name, dir_b_num_files, 
 											  copy_from_a, copy_to_b);
 
-	char* copy_from_b[MAX_NUM_FILES]; // Which files should we copy from b to a
+	// Which files should we copy from b to a
+	char* copy_from_b[MAX_NUM_FILES]; 	
 	char* copy_to_a[MAX_NUM_FILES];
-	int size_b_to_a = Files_To_Copy(dir_b_files, dir_b_files_full_path, dir_b_num_files, 
-											  dir_a_files, dir_a_files_full_path, dir_a_num_files,
+	int size_b_to_a = Files_To_Copy(dir_b_files, dir_b_name, dir_b_num_files, 
+											  dir_a_files, dir_a_name, dir_a_num_files,
 											  copy_from_b, copy_to_a);
 
+	// Synchronize files -- copy from directory a to directory b
 	for(int i = 0; i < size_a_to_b; i++)
 	{
 		fprintf(stdout, "Copy from %s to %s\n", copy_from_a[i], copy_to_b[i]);
+		Copy_From_To(copy_from_a[i], copy_to_b[i]);
 	}
 
+	// Synchronize files -- copy from directory b to directory a
 	for(int i = 0; i < size_b_to_a; i++)
 	{
 		fprintf(stdout, "Copy from %s to %s\n", copy_from_b[i], copy_to_a[i]);
+		Copy_From_To(copy_from_b[i], copy_to_a[i]);
 	}
-
 
 	exit(0);
-}
-
-
-void Get_Dir_From_Path(char* path, char* delim, char* out)
-{
-	strcpy(out, path);
-	
-	int curr_loc = 0;
-	char* start_str = &out[curr_loc];
-
-	while( strstr(start_str, delim) != NULL )
-	{
-		curr_loc++;
-		start_str = &out[curr_loc];
-	}
-	
-	out[curr_loc] = '\0';
-}
-
-
-void Get_File_From_Path(char* path, char* delim, char* out)
-{
-	strcpy(out, path);
-	
-	int curr_loc = 0;
-	char* start_str = &out[curr_loc];
-
-	while( strstr(start_str, delim) != NULL )
-	{
-		curr_loc++;
-		start_str = &out[curr_loc];
-	}
-	strcpy(out, start_str);	
 }
 
 
@@ -159,68 +118,59 @@ void Copy_From_To(char* from, char* to)
 			
 }
 
-int Files_To_Copy(char** from_dir_files, char** from_dir_full_path, int from_dir_size, 
-					   char** to_dir_files, char** to_dir_full_path, int to_dir_size, 
+int Files_To_Copy(char** from_dir_files, char* from_dir, int from_dir_size, 
+					   char** to_dir_files, char* to_dir, int to_dir_size, 
 						char** from_out, char** to_out)
 {
 	int c = 0;
 
-	char* to_dir;
-	// Get to_dir name
-	if( to_dir_size > 0 )
-	{
-		to_dir = malloc( strlen(to_dir_full_path[0]) + 1 );
-		Get_Dir_From_Path(to_dir_full_path[0], "/", to_dir);
-//		printf("%s\n", to_dir_full_path[0]);	
-	}
-
 	// Loop through `from_dir_files`
 	for(int i = 0; i < from_dir_size; i++)
 	{
+		char* from = malloc( strlen(from_dir_files[i]) + strlen(from_dir) + 1);			
+		from[0] = '\0';
+		strcat(from, from_dir); 
+		strcat(from, from_dir_files[i]);
+
+		char* to = malloc( strlen(to_dir) + strlen(from_dir_files[i]) + 1 );
+		to[0] = '\0';
+		strcat(to, to_dir);
+		strcat(to, from_dir_files[i]);
+	
 		// If file in `from` doesn't exist in `to`
 		if( ! File_Exists(from_dir_files[i], to_dir_files, to_dir_size) )
 		{
-			from_out[c] = malloc( strlen(from_dir_full_path[i]) + 1);
-			strcpy(from_out[c], from_dir_full_path[i]);
+			from_out[c] = malloc( strlen(from) + 1 );
+			strcpy(from_out[c], from);
 	
-			char* tmp = malloc( strlen(to_dir) + strlen(from_dir_files[i]) + 1 );
-			tmp[0] = '\0';
-			strcat(tmp, to_dir);
-			strcat(tmp, from_dir_files[i]);
-			to_out[c] = malloc( strlen(tmp) + 1 );
-			strcpy(to_out[c], tmp);
-			free(tmp);
+			to_out[c] = malloc( strlen(to) + 1 );
+			strcpy(to_out[c], to);
 
 			c++;
 		}
 		else // File exists in `from` and `to`
 		{
 			struct stat from_file;
-			stat(from_dir_full_path[i], &from_file);
+			stat(from, &from_file);
 
 			struct stat to_file;
-			stat(to_dir_full_path[i], &to_file);
+			stat(to, &to_file);
 
 			// If `from_file` was modified earlier than `to_file`, copy `from`
 			// to `to`
 			if( difftime(from_file.st_mtime, to_file.st_mtime) > 0 )
 			{
-				from_out[c] = malloc( strlen(from_dir_full_path[i]) + 1 );
-				strcpy(from_out[c], from_dir_full_path[i]);
+				from_out[c] = malloc( strlen(from) + 1 );
+				strcpy(from_out[c], from);
 				
-				char* tmp = malloc( strlen(to_dir) + strlen(from_dir_files[i]) + 1 );
-				tmp[0] = '\0';
-				strcat(tmp, to_dir);
-				strcat(tmp, from_dir_files[i]);
-				to_out[c] = malloc( strlen(tmp) + 1 );
-				strcpy(to_out[c], tmp);
-				free(tmp);
+				to_out[c] = malloc( strlen(to) + 1 );
+				strcpy(to_out[c], to);
 	
 				c++;
 			}			
 		}
+		free(to); free(from);
 	}
-	free(to_dir);
 	return c;
 }
 
