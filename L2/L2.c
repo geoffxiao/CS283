4234e9117cf1e38c5535749f785d55404cef4344
geoffxiao <geoffreyxiao96@gmail.com>
2018-02-17 13:21:28 -0500

    L2.c

--- L2/L2.c
+++ L2/L2.c
@@ -26,15 +26,13 @@
 
 #define MAX_NUM_FILES 2048 // Maximum number of files in a directory
 
-DIR* Open_Dir(char* dir_name);
-int Get_Files(DIR* dir, char** files);
-void Get_Full_Path(char* directory, char** files, int num_files, char** files_out);
-int Get_Reg_Files(char* directory, char** files, int num_files);
-bool File_Exists(char* file, char** array, int array_size);
-int Files_To_Copy(char** from_dir_files,char* from_dir, int from_dir_size, 
-					   char** to_dir_files, char* to_dir, int to_dir_size, 
-						char** from_out, char** to_out);
-void Copy_From_To(char* from, char* to);
+DIR* Open_Dir(char* dir_name); // Open directory stream
+int Get_Files(DIR* dir, char** files); // Get all the files in a directory
+int Get_Reg_Files(char* directory, char** files, int num_files); // Only get the regular files in a directory
+bool File_Exists(char* file, char** array, int array_size); // Does `file` exist in `array`?
+char* Full_Path(char* dir, char* file); // Full path for a file
+void Delete_File(char* file); // Delete file
+void Copy_From_To(char* from, char* to); // Copy a file from `from` to `to`
 
 
 int main(int argc, char** argv)
@@ -65,60 +63,159 @@ int main(int argc, char** argv)
 	// Get files in each directory
 	int dir_a_num_files = Get_Files(dir_a, dir_a_files); 
 	int dir_b_num_files = Get_Files(dir_b, dir_b_files);
-	
 	dir_a_num_files = Get_Reg_Files(dir_a_name, dir_a_files, dir_a_num_files);	
 	dir_b_num_files = Get_Reg_Files(dir_b_name, dir_b_files, dir_b_num_files);	
 
-	// Which files should we copy from a to b
-	char* copy_from_a[MAX_NUM_FILES]; 	
-	char* copy_to_b[MAX_NUM_FILES];
-	int size_a_to_b = Files_To_Copy(dir_a_files, dir_a_name, dir_a_num_files, 
-											  dir_b_files, dir_b_name, dir_b_num_files, 
-											  copy_from_a, copy_to_b);
-
-	// Which files should we copy from b to a
-	char* copy_from_b[MAX_NUM_FILES]; 	
-	char* copy_to_a[MAX_NUM_FILES];
-	int size_b_to_a = Files_To_Copy(dir_b_files, dir_b_name, dir_b_num_files, 
-											  dir_a_files, dir_a_name, dir_a_num_files,
-											  copy_from_b, copy_to_a);
-
-	// Synchronize files -- copy from directory a to directory b
-	for(int i = 0; i < size_a_to_b; i++)
+	// Compare files in directory `a` and directory `b` to see how to sync files
+	// Make an array to keep track
+	//		-1 = delete the file
+	//		+1 = copy the file into directory `a`
+	//		+2 = copy the file into directory `b`
+	//		 0 = do nothing
+	
+	int DELETE_FILE = -1;
+	int CP_TO_A = 1;
+	int CP_TO_B = 2;
+	
+	int dir_a_sync[dir_a_num_files] = {0};
+	int dir_b_sync[dir_b_num_files] = {0};
+	
+	// If file in `a` doesn't exist in `b`, copy the file in `a` to `b`
+	for(int i = 0; i < dir_a_num_files; i++)
 	{
-		fprintf(stdout, "Copy from %s to %s\n", copy_from_a[i], copy_to_b[i]);
-		Copy_From_To(copy_from_a[i], copy_to_b[i]);
+		if( ! File_Exists(dir_a_files[i], dir_b_files, dir_b_num_files) )
+		{
+			// We will need to copy the file in `a` to `b`
+			dir_a_sync[i] = CP_TO_B;
+		}
 	}
-
-	// Synchronize files -- copy from directory b to directory a
-	for(int i = 0; i < size_b_to_a; i++)
+	
+	// If file in `b` doesn't exist in `a`, delete the file in `b`
+	for(int i = 0; i < dir_b_num_files; i++)
 	{
-		fprintf(stdout, "Copy from %s to %s\n", copy_from_b[i], copy_to_a[i]);
-		Copy_From_To(copy_from_b[i], copy_to_a[i]);
-	}
+		if( ! File_Exists(dir_b_files[i], dir_a_files, dir_a_num_files) )
+		{
+			// We will need to delete the file from `b`
+			dir_b_sync[i] = DELETE_FILE;
+		}
+	}	
+	
+	// If the file in `b` exists in `a`
+	for(int i = 0; i < dir_a_num_files; i++)
+	{
+		if( File_Exists(dir_a_files[i], dir_b_files, dir_b_num_files) )
+		{
+			// We will need to see which file was modified more recently
+			struct stat file_in_a;
+			stat(dir_a_files[i], &file_in_a);
 
+			struct stat file_in_b;
+			stat(dir_b_files[i], &file_in_b);
+			
+			// If `file_in_a` was modified more recently than `file_in_b`
+			// Copy the file in `a` to `b`
+			if( difftime(file_in_a.st_mtime, file_in_b.st_mtime) > 0 )
+			{
+				dir_a_sync[i] = CP_TO_B; // copy file in `a` to `b`
+			}
+			else
+			{
+				dir_b_sync[i] = CP_TO_A; // copy file in `b` to `a`
+			}
+		}
+	}
+	
+	// Now do the specified operations
+	
+	// Look at all the files in a
+	for(int i = 0; i < dir_a_num_files; i++)
+	{
+		// Delete the file from `a`
+		if(dir_a_sync[i] == DELETE_FILE)
+		{
+			char* full_path = Full_Path(dir_a_name, dir_a_files[i]);
+			Delete_File(full_path);
+			free(full_path);
+			
+			fprintf(stdout, "Deleted %s\n", full_path);
+		}
+		
+		// Copy the file from `a` to `b`
+		else if(dir_a_sync[i] == CP_TO_B)
+		{
+			char* cp_from = Full_Path(dir_a_name, dir_a_files[i]);
+			char* cp_to = Full_Path(dir_b_name, dir_a_files[i]);
+			Copy_From_To(cp_from, cp_to);
+			free(cp_from); free(cp_to);
+			
+			fprintf(stdout, "Copied %s to %s\n", cp_from, cp_to);
+		}
+	}
+	
+	// Look at all the files in b
+	for(int i = 0; i < dir_b_num_files; i++)
+	{
+		// Delete the file from `b`
+		if(dir_b_sync[i] == DELETE_FILE)
+		{
+			char* full_path = Full_Path(dir_b_name, dir_b_files[i]);
+			Delete_File(full_path);
+			free(full_path);
+			
+			fprintf(stdout, "Deleted %s\n", full_path);
+		}
+		
+		// Copy the file from `b` to `a`
+		else if(dir_b_sync[i] == CP_TO_A)
+		{
+			char* cp_from = Full_Path(dir_b_name, dir_b_files[i]);
+			char* cp_to = Full_Path(dir_a_name, dir_b_files[i]);
+			Copy_From_To(cp_from, cp_to);
+			free(cp_from); free(cp_to);
+			
+			fprintf(stdout, "Copied %s to %s\n", cp_from, cp_to);
+		}
+	}	
+	
 	exit(0);
 }
 
+// Create full path from directory name and file name. Returns dynamically
+// allocated C string.
+char* Full_Path(char* dir, char* file)
+{
+	char* out = malloc( strlen(dir) + strlen(file) + 1 );
+	strcpy(out, dir); 
+	strcat(out, file);
+	return out;
+}
+
+// Delete file specified 
+void Delete_File(char* file)
+{
+	if(remove(file) != 0)
+	{
+		fprintf(stderr, "Unable to Delete File%s\n", file);
+	}
+}
 
 
-
-// Copy file `from` to directory `to`
+// Copy file `from` to `to`. Copy file contents and file permissions
 void Copy_From_To(char* from, char* to)
 {
 	// Get file permissions of `from` file
 	// Copy these file permissions
 	struct stat from_file_permissions; stat(from, &from_file_permissions);
 
+	// Open files, check for errors
 	FILE* from_file = fopen(from, "r");
 	FILE* to_file = fopen(to, "w+");
-
 	if(from_file == NULL)
-		perror("ERROR\n");
+		fprintf(stderr, "Error Opening %s\n", from);
 
 	if(to_file == NULL)
 	{
-		perror("ERROR\n");
+		fprintf(stderr, "Error Opening %s\n", to);
 	}
 
 	char buffer[256];
@@ -134,66 +231,11 @@ void Copy_From_To(char* from, char* to)
 	fclose(from_file); fclose(to_file); // close FILEs
 
 	// copy file permissions	
-	chmod(to, from_file_permissions.st_mode & 07777);
+	if( chmod(to, from_file_permissions.st_mode & 07777) == -1 )
+		fprintf(stderr, "Error Changing File Permissions for %s\n", to);
 }
 
-int Files_To_Copy(char** from_dir_files, char* from_dir, int from_dir_size, 
-					   char** to_dir_files, char* to_dir, int to_dir_size, 
-						char** from_out, char** to_out)
-{
-	int c = 0;
-
-	// Loop through `from_dir_files`
-	for(int i = 0; i < from_dir_size; i++)
-	{
-		char* from = malloc( strlen(from_dir_files[i]) + strlen(from_dir) + 1);			
-		from[0] = '\0';
-		strcat(from, from_dir); 
-		strcat(from, from_dir_files[i]);
-
-		char* to = malloc( strlen(to_dir) + strlen(from_dir_files[i]) + 1 );
-		to[0] = '\0';
-		strcat(to, to_dir);
-		strcat(to, from_dir_files[i]);
-	
-		// If file in `from` doesn't exist in `to`
-		if( ! File_Exists(from_dir_files[i], to_dir_files, to_dir_size) )
-		{
-			from_out[c] = malloc( strlen(from) + 1 );
-			strcpy(from_out[c], from);
-	
-			to_out[c] = malloc( strlen(to) + 1 );
-			strcpy(to_out[c], to);
-
-			c++;
-		}
-		else // File exists in `from` and `to`
-		{
-			struct stat from_file;
-			stat(from, &from_file);
-
-			struct stat to_file;
-			stat(to, &to_file);
-
-			// If `from_file` was modified earlier than `to_file`, copy `from`
-			// to `to`
-			if( difftime(from_file.st_mtime, to_file.st_mtime) > 0 )
-			{
-				from_out[c] = malloc( strlen(from) + 1 );
-				strcpy(from_out[c], from);
-				
-				to_out[c] = malloc( strlen(to) + 1 );
-				strcpy(to_out[c], to);
-	
-				c++;
-			}			
-		}
-		free(to); free(from);
-	}
-	return c;
-}
-
-
+// Does the `file` exist in `array`?
 bool File_Exists(char* file, char** array, int array_size)
 {
 	for(int i = 0; i < array_size; i++)
@@ -235,31 +277,6 @@ int Get_Reg_Files(char* directory, char** files, int num_files)
 	}
 	return c;
 }
-
-
-// Get the full path name of the files in `files`. The `files` live in
-// directory `directory`. Store full path names in `files_out`
-void Get_Full_Path(char* directory, char** files, int num_files, char** files_out)
-{
-	char* tmp;
-	struct stat check_file;
-
-	// Get full path names
-	// Store in files_out
-	for(int i = 0; i < num_files; i++)
-	{
-		tmp = malloc( strlen(directory) + strlen(files[i]) + 1 );
-		tmp[0] = '\0';
-		strcat(tmp, directory);
-		strcat(tmp, files[i]);
-		stat(tmp, &check_file);	
-	
-		files_out[i] = malloc( strlen(tmp) + 1 );
-		strcpy(files_out[i], tmp); // Change file names to full path name
-		free(tmp);
-	}
-}
-
 
 // Get all files in `DIR* dir`, save file names in `files` array
 int Get_Files(DIR* dir, char** files)
